78ad2e3ce0e3aaee3956079e77b5352dcb5288a1
Eric Wilson <ericswilson94@gmail.com>
2018-11-22 06:09:39 +0000

    Update github file for lightcoin-crypto

--- index.js
+++ index.js
@@ -1,29 +1,78 @@
-let balance = 500.00;
+// let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    let total = 0;
+    this.transactions.forEach((input) => {
+      total += input.value;
+    })
+     return total;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+
+    this.time = new Date();
+
+    if (this.account.balance + this.value >= 0){
+      this.account.addTransaction(this);
+    }
+    else {
+      console.log("You do not have the necessary funds.");
+    }
+  }
+}
+
+// GOOD
+class Deposit extends Transaction {
+
+  get value() {
+  return this.amount;
   }
 
 }
 
+// GOOD
+class Withdrawal extends Transaction {
 
+  get value() {
+    return -this.amount;
+  }
 
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
+
+const t3 = new Withdrawal(200.00, myAccount);
+t3.commit();
+
+console.log("T3 Balance:", myAccount.balance)
